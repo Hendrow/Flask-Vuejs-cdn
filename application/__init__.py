b0080de27ea745e5ea6abc6dc1a78f23b8f10e5e
Hendrow <wijayahendro@gmail.com>
2024-06-13 15:54:19 +0700

    route add barang

--- application/__init__.py
+++ application/__init__.py
@@ -19,6 +19,7 @@ def create_app():
     migrate.init_app(app, db)
 
     from .models import Barang
+    from .forms import FormBarang
 
     @app.route('/')
     def home():
@@ -26,5 +27,24 @@ def create_app():
         if request.headers.get('X-Requested-Width') =='XMLHttpRequest':
             return jsonify(daftarBarang), 200
         return render_template('latihan.html')
+    
+    @app.route('/add', methods=['POST'])
+    def add_barang():
+        user_input = request.get_json()
+        form = FormBarang(data=user_input)
+
+        namaBarang = form.namaBarang.data
+        merk = form.merk.data
+        tipe = form.tipe.data
+        sn = form.sn.data
+        status = form.status.data
+
+        if form.validate():
+            barang = Barang(namaBarang=namaBarang, merk=merk, tipe=tipe, sn=sn, status=status)
+            db.session.add(barang)
+            db.session.commit()
+
+            return jsonify(barang)
+        return redirect(url_for('home'))
     
     return app
